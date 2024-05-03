54b35278f9b28b556cb5930099b5bdffb8648d75
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-05-03 20:21:35 +0700

    Ep-125-remember-login-user-state

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,12 +1,16 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const AuthContext = createContext();
 
 let AuthReducer = (state, action) => {
     switch (action.type) {
         case "LOGIN":
+            //store user in localstorage
+            localStorage.setItem('user', JSON.stringify(action.payload))
             return { user: action.payload };
         case "LOGOUT":
+            //remove user in localstorage
+            localStorage.removeItem('user');
             return { user: null };
         default:
             return state;
@@ -17,6 +21,19 @@ const AuthContextProvider = ({ children }) => {
     let [state, dispatch] = useReducer(AuthReducer, {
         user: null
     });
+
+    useEffect(() => {
+        try {
+            let user = JSON.parse(localStorage.getItem('user'));//json string
+            if (user) {
+                dispatch({ type: 'LOGIN', payload: user })
+            } else {
+                dispatch({ type: "LOGOUT" });
+            }
+        } catch (e) {
+            dispatch({ type: "LOGOUT" });
+        }
+    }, [])
 
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
