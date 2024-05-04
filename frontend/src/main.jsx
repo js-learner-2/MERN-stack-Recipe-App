04407608b78621ffa4b70b3562bad688f865f534
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-05-04 23:20:49 +0700

    Ep-126-Protect-React-Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,61 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Contact from './pages/Contact.jsx';
-import RecipeForm from './pages/RecipeForm.jsx';
-import SignUpForm from './pages/SignUpForm.jsx';
-import SignInForm from './pages/SignInForm.jsx';
 import { AuthContextProvider } from './contexts/AuthContext.jsx';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import Routes from './routes/index.jsx';
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/", //http://localhost:5173/
-        element: <Home />
-      },
-      {
-        path: "/about", //http://localhost:5173/about
-        element: <About />
-      },
-      {
-        path: "/contact", //http://localhost:5173/contact
-        element: <Contact />
-      },
-      {
-        path: "/recipes/create", //http://localhost:5173/recipes/create
-        element: <RecipeForm />
-      },
-      {
-        path: "/recipes/edit/:id", //http://localhost:5173/recipes/edit/id
-        element: <RecipeForm />
-      },
-      {
-        path: "/sign-in", //http://localhost:5173/sign-in
-        element: <SignInForm />
-      },
-      {
-        path: "/sign-up", //http://localhost:5173/sign-up
-        element: <SignUpForm />
-      },
-    ]
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <Routes />
     </AuthContextProvider>
   </React.StrictMode>,
 )
